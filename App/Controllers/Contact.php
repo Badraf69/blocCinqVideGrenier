3a8aa01c378df6606bede84ad58e285c886f4d58
Masselot <101673780+Badraf69@users.noreply.github.com>
2025-09-25 18:17:04 +0200

    MLT-250925- env de prod

--- App/Controllers/Contact.php
+++ App/Controllers/Contact.php
@@ -1,43 +1,4 @@
 <?php
-//
-//namespace App\Controllers;
-//
-//use App\Models\Articles;
-//use \Core\View;
-//use Exception;
-//
-//class Contact extends \Core\Controller
-//{
-//    /**
-//     * Affiche le formulaire de contact
-//     */
-//    public function formAction()
-//    {
-//        View::renderTemplate('Show/form.html');
-//    }
-//
-//    /**
-//     * Traite l’envoi du formulaire
-//     */
-//    public function sendAction()
-//    {
-//        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//            $to = "admin@bloc-cinq.local";
-//            $subject = "Message depuis formulaire de contact";
-//            $message = "Nom: " . $_POST['contact_name'] . "\nEmail: " . $_POST['contact_email'] . "\nMessage: " . $_POST['contact_message'];
-//            $headers = "From: noreply@bloc-cinq.local";
-//
-//            if (mail($to, $subject, $message, $headers)) {
-//                echo "Message envoyé !";
-//            } else {
-//                echo "Erreur lors de l'envoissss.";
-//            }
-//        }
-//    }
-//}
-//
-
-
 namespace App\Controllers;
 
 use \Core\View;
@@ -50,29 +11,24 @@ class Contact extends \Core\Controller
     {
         View::renderTemplate('Show/form.html');
     }
-
     public function sendAction()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $mail = new PHPMailer(true);
 
             try {
-
-                // SMTP Mailpit
+                // SMTP Mailpità
                 $mail->isSMTP();
                 $mail->Host = 'mailpit';
                 $mail->Port = 1025;
                 $mail->SMTPAuth = false;
-                $mail->SMTPSecure = ''; // pas false
-
+                $mail->SMTPSecure = '';
                 $mail->setFrom('noreply@bloc-cinq.local', 'Bloc-Cinq');
                 $mail->addAddress('admin@bloc-cinq.local');
-
                 $mail->Subject = 'Message depuis formulaire de contact';
                 $mail->Body = "Nom: " . $_POST['contact_name'] .
                     "\nEmail: " . $_POST['contact_email'] .
                     "\nMessage: " . $_POST['contact_message'];
-
                 $mail->send();
                 View::renderTemplate('Home/index.html', []);
                 exit;
