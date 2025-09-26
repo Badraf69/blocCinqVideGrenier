85898ea5b5051d04c48063dc5f667ba9c9352544
Masselot <101673780+Badraf69@users.noreply.github.com>
2025-09-26 12:25:18 +0200

    MLT-260925- swagger

--- public/swagger/swagger-initializer.js
+++ public/swagger/swagger-initializer.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     window.ui = SwaggerUIBundle({
-        url: "./openapi.yaml", // notre spec locale
+        url: "./openapi.yaml",
         dom_id: '#swagger-ui',
         deepLinking: true,
         presets: [
